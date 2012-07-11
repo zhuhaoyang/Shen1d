19319b82c61e754c98f77c16cc60d52f2fdcf71f
Peter Zhu <jy01902848@akematoiMac.local>
2012-07-11 12:47:16 +0800

    Initial Commit

---
+++ shen1d/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  shen1d
+//
+//  Created by Peter Zhu on 12-7-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ shen1d/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  shen1d
+//
+//  Created by Peter Zhu on 12-7-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
